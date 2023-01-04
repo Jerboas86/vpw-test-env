01ba02ff982b9b5d35f761aad841222a0eff082b
Jerboas86 <benoitdelemps@gmail.com>
2023-01-04 09:15:25 +0100

    add vitest scope

--- apps/wasm-svelte/src/lib/window/Window.test.ts
+++ apps/wasm-svelte/src/lib/window/Window.test.ts
@@ -1,1 +1,8 @@
+import { describe, expect, it } from "vitest";
+import { add } from "@vpwtest/tester";
 
+describe("vite-wasm-plugin", () => {
+  it("checks if the plugin works with vitest", () => {
+    expect(add(1, 3)).toBe(4);
+  });
+});
--- apps/wasm-svelte/vite.config.ts
+++ apps/wasm-svelte/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import wasm from "vite-plugin-wasm";
@@ -6,6 +7,9 @@ import topLevelAwait from "vite-plugin-top-level-await";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), wasm(), topLevelAwait()],
+  test: {
+    environment: "jsdom",
+  },
   worker: {
     format: "es",
     plugins: [wasm(), topLevelAwait()],
